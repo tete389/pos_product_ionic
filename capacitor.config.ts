1546e6a98bdd19bddc5ad15355f0e171409e0cdf
tete389 <tete389@gmail.com>
2024-05-04 02:22:05 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pos_alone_side_menu',
+  webDir: 'www'
+};
+
+export default config;
