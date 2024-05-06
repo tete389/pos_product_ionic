f23887d277846603c3f97cf6e3ca38c85be90ac7
tete389 <tete389@gmail.com>
2024-05-06 18:07:22 +0700

    update_route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'dashboard',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.page').then( m => m.DashboardPage)
   },
 ];
