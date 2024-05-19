a9ec620781c7bd4877eee2b06dc4dbf105ebfaba
kornkawee <kornkawee.non11@gmail.com>
2024-05-19 21:29:45 +0700

    updaet style menu-managment

--- src/app/menu-managment/menu-managment.page.ts
+++ src/app/menu-managment/menu-managment.page.ts
@@ -1,20 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent,IonButtons,IonMenuButton, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  IonContent,
+  IonButtons,
+  IonButton,
+  IonMenuButton,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonSearchbar,
+  IonSelect,
+  IonSelectOption,
+  IonCard,
+  IonCardContent
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-menu-managment',
   templateUrl: './menu-managment.page.html',
   styleUrls: ['./menu-managment.page.scss'],
   standalone: true,
-  imports: [IonContent,IonButtons,IonMenuButton, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonButtons,
+    IonButton,
+    IonMenuButton,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonLabel,
+    IonSearchbar,
+    IonSelect,
+    IonSelectOption,
+    IonCard,
+    IonCardContent
+  ],
 })
 export class MenuManagmentPage implements OnInit {
+  customPopoverOptions = {
+    cssClass: 'my-custom-customPopover-printer',
+  };
 
-  constructor() { }
+  select_tab_menu: number = 0;
+  searchControl: FormControl;
+  search_text = '';
+  placho_text = 'ค้นหา';
 
-  ngOnInit() {
+  constructor() {
+    this.searchControl = new FormControl();
   }
 
+  ngOnInit() {}
+
+  onCancel() {}
+
+  onSearchInput(text: string) {}
+
+  search_enter(val: any) {
+    console.log(typeof val);
+  }
 }
