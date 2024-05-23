35e1ab3b9b2a6d3bab56cbe3d5118db405228db1
Zxx1140 <zixzax4545@gmail.com>
2024-05-24 00:17:06 +0700

    card-แสดงผล

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -2,16 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonCard,IonCol,IonRow,IonContent, IonHeader, IonTitle, IonToolbar , IonGrid} from '@ionic/angular/standalone';
-
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
   standalone: true,
-  imports: [IonCard,IonCol,IonRow,IonContent,IonGrid, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [NgFor,IonCard,IonCol,IonRow,IonContent,IonGrid, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class DashboardPage implements OnInit {
-
+  items = [1, 2, 3, 4,6,7,8,9,10,11,12];
   constructor() { }
 
   ngOnInit() {
