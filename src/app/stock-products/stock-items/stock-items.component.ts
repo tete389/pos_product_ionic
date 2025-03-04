8479c72998d9f84aa58d1ca84a6f59c6679adc9d
Zxx1140 <zixzax4545@gmail.com>
2025-03-04 22:10:09 +0700

    zaku

--- src/app/stock-products/stock-items/stock-items.component.ts
+++ src/app/stock-products/stock-items/stock-items.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-stock-items',
+  standalone: true,
+  imports: [CommonModule, IonicModule], // ✅ Import IonicModule สำหรับ Ionic Components
   templateUrl: './stock-items.component.html',
   styleUrls: ['./stock-items.component.scss'],
-  standalone: true,
 })
-export class StockItemsComponent  implements OnInit {
+export class StockItemsComponent implements OnInit {
 
+
+  activeButton: string = ''; // เก็บค่าปุ่มที่ถูกคลิก
+  items = [
+    {
+      image: 'assets/images/eggs.png',
+      name: 'ไข่ไก่',
+      category: 'Meat',
+      unit: 'Kg',
+      maxLevel: 100,
+      reorderPoint: '> 20%',
+      orderQuantity: 100,
+      location: 'B1',
+      supplier: 'Makro',
+      price: 250,
+    },
+    {
+      image: 'assets/images/beef.png',
+      name: 'สันในวัว',
+      category: 'Meat',
+      unit: 'Kg',
+      maxLevel: 5,
+      reorderPoint: '> 20%',
+      orderQuantity: 100,
+      location: 'B1',
+      supplier: 'Makro',
+      price: 250,
+    },
+    {
+      image: 'assets/images/s.png',
+      name: 'กุ้งสด',
+      category: 'ซีฟู้ด',
+      unit: 'Kg',
+      maxLevel: 5,
+      reorderPoint: '> 20%',
+      orderQuantity: 100,
+      location: 'B1',
+      supplier: 'Makro',
+      price: 250,
+    }
+  ];
   constructor() { }
 
-  ngOnInit() {}
+
+
+
+  setActive(buttonName: string) {
+    this.activeButton = buttonName;
+  }
+  ngOnInit() { }
 
 }
